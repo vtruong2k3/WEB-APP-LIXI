4f85f7e900d702da3a9c896812de229e680ae364
vtruong2k3 <vtruong2k3@gmail.com>
2025-01-25 14:15:24 +0700

    lixi

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+const usedNames = new Set();
+const rewards = ["10k", "20k", "50k", "100k", "200k"];
+
+document.getElementById("startBtn").addEventListener("click", () => {
+    const nameInput = document.getElementById("nameInput").value.trim();
+
+    if (!nameInput) {
+        alert("Vui lòng nhập tên của bạn!");
+        return;
+    }
+
+    if (usedNames.has(nameInput)) {
+        alert("Tên này đã lắc rồi, hãy thử tên khác!");
+        return;
+    }
+
+    document.getElementById("formContainer").classList.add("hidden");
+    document.getElementById("lixiAnimation").classList.remove("hidden");
+
+    setTimeout(() => {
+        const randomReward = rewards[Math.floor(Math.random() * rewards.length)];
+        usedNames.add(nameInput);
+
+        document.getElementById("lixiAnimation").classList.add("hidden");
+        document.getElementById("result").classList.remove("hidden");
+        document.getElementById('name').textContent=nameInput;
+        document.getElementById("lixiAmount").textContent = randomReward;
+    }, 3000);
+});
